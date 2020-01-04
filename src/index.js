51bf34dedb36e551aff7affe3c82366bf7a760df
evgar <eugene.garichev@gmail.com>
2020-01-04 14:13:55 +0200

    Add Routing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from 'react-router-dom'
 
 // 1  Importing the required dependencies from the installed packages.
 import { ApolloProvider } from "react-apollo";
@@ -23,9 +24,11 @@ const client = new ApolloClient({
 
 // 4  Render the root component of your React app. The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop.
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>,
   document.getElementById("root")
 );
 serviceWorker.unregister();
